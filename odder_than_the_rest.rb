a1fe4caf6016f4750ae58af0e5889655483e80cc
David Morton <55235981+davidmorton0@users.noreply.github.com>
2019-12-06 11:42:29 +0000

    first commit

---
+++ odder_than_the_rest.rb
@@ -0,0 +1,24 @@
+def oddest(arr)
+	if arr == [] then return nil end
+	oddness_counter = arr.map { |val| oddness_calculator(val)}
+	most_odd = oddness_counter.max
+	if oddness_counter.count(-1) > 1
+		return nil
+	elsif oddness_counter.count(-1) == 1
+		return -1
+	elsif oddness_counter.count(most_odd) > 1
+		return nil
+	else
+		return arr[oddness_counter.index(most_odd)]
+	end
+end
+
+def oddness_calculator(val)
+	if val == -1
+		return -1
+	elsif val % 2 == 0
+		return 0
+	else
+		return 1 + oddness_calculator((val - 1) / 2)
+	end
+end
